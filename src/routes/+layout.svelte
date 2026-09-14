4720d67440185cfda44b75d50eea9d7c32df5f2c
Hallvord R. M. Steen <hallvord@minus.no>
2026-09-14 13:56:23 +0200

    Organ pipe favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
+	import favicon16 from '$lib/assets/favicon-16x16.png';
+	import favicon32 from '$lib/assets/favicon-32x32.png';
+	import appleTouchIcon from '$lib/assets/apple-touch-icon.png';
+	import androidChrome192 from '$lib/assets/android-chrome-192x192.png';
+	import androidChrome512 from '$lib/assets/android-chrome-512x512.png';
 	import { onNavigate } from '$app/navigation';
 
 	let { children } = $props();
@@ -17,7 +22,12 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={favicon} sizes="any" />
+	<link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+	<link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+	<link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+	<link rel="icon" type="image/png" sizes="192x192" href={androidChrome192} />
+	<link rel="icon" type="image/png" sizes="512x512" href={androidChrome512} />
 </svelte:head>
 {@render children?.()}
 
